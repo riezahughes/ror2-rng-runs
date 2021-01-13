e2bdced079f23eae720c8e07bc522ea3dae89755
riezahughes <dan@ekwholesale.co.uk>
2021-01-13 11:07:42 +0000

    Stuff set. still to do post

--- api/src/server.js
+++ api/src/server.js
@@ -1,5 +1,41 @@
-// start server
+require('dotenv').config()
 
-// when you use the /roll endpoint, settings will be rng'd based on true/false post requests
+// include json files
+const artifacts = require('./resources/artifacts.json');
+const characters = require('./resources/characters.json');
 
-// use a json file to keep a list of all characters, artifacts and skills.
+const express = require('express')
+const app = express()
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/roll', (req, res) => {
+    // request items will be broken down for each individual artifact, then rng will pick from the character and/or artifact list and return a result.
+    /*
+
+    example request: 
+
+    {
+        artifacts: [
+            "chaos",
+            "command"
+        ]
+
+        characters: [
+            "captain",
+            "huntress"
+        ]
+    }
+
+    */
+
+    
+    res.send('posted!')
+});
+
+app.listen(port, () => {
+  console.log(`There are ${artifacts.length} Artifacts and ${characters.length} characters ready to pick from. Running on port ${port}`)
+})
