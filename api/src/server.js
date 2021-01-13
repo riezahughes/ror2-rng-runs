ac29f81ff8bcf8d8043a64e8589151f306d908b4
riezahughes <dan@ekwholesale.co.uk>
2021-01-13 05:19:37 +0000

    starting the thing!

---
+++ api/src/server.js
@@ -0,0 +1,5 @@
+// start server
+
+// when you use the /roll endpoint, settings will be rng'd based on true/false post requests
+
+// use a json file to keep a list of all characters, artifacts and skills.
