9971563a9ebd2063db8fd89744f90c48a17fb03f
riezahughes <dan@ekwholesale.co.uk>
2021-01-14 10:05:00 +0000

    Added correct rng to pull from the api for all needs.

--- api/src/server.js
+++ api/src/server.js
@@ -2,38 +2,90 @@ require('dotenv').config();
 
 // include json files
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const artifacts = require('./resources/artifacts.json');
 const characters = require('./resources/characters.json');
 
 const app = express();
+
+const jsonParser = bodyParser.json();
+
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.post('/roll', (req, res) => {
-  /*
-  request items will be broken down for each individual artifact,
-  rng will pick from the character and/or artifact list and return a result.
+app.post('/roll', jsonParser, (req, res) => {
+  console.log(req.body.action);
+  console.log(req.body.filters.artifacts);
+  if (req.body.action !== 'roll') {
+    res.send(
+      {
+        status: 'failed',
+        code: 422,
+      },
+    ).status(422);
+  } else {
+    // run a map on the full artifact json and then filter to remove the results we don't need.
+    const filteredArtifactsResult = artifacts.map((artifact) => {
+      if (req.body.filters.artifacts && !req.body.filters.artifacts.includes(artifact.name)) {
+        return (artifact);
+      }
+    }).filter((e) => e);
 
-    example request:
+    const filteredCharactersResult = characters.map((character) => {
+      if (req.body.filters.characters && !req.body.filters.characters.includes(character.name.toLowerCase())) {
+        return (character);
+      }
+    }).filter((e) => e);
 
-    {
-        artifacts: [
-            "chaos",
-            "command"
-        ]
+    let artifactFinalResult;
 
-        characters: [
-            "captain",
-            "huntress"
-        ]
+    if (req.body.filters.artifactLimit && req.body.filters.artifactLimit <= 16 && req.body.filters.artifactLimit > 0) {
+      const { artifactLimit } = req.body.filters;
+      const artifactArray = [];
+      while (artifactArray.length !== artifactLimit) {
+        const choice = Math.floor(Math.random() * Math.floor(filteredArtifactsResult.length));
+        if (!artifactArray.includes(filteredArtifactsResult[choice])) {
+          console.log(`Number: ${choice}`);
+          console.log(`pushing ${filteredArtifactsResult[choice].name}`);
+          artifactArray.push(filteredArtifactsResult[choice]);
+        }
+      }
+      artifactFinalResult = artifactArray;
+    } else {
+      artifactFinalResult = filteredArtifactsResult;
     }
 
-  */
-  res.send('posted!');
+    let characterFinalResult;
+
+    if (req.body.filters.characterLimit && filteredCharactersResult.length !== 0 && req.body.filters.characterLimit <= 4 && req.body.filters.characterLimit > 0) {
+      const { characterLimit } = req.body.filters;
+      const characterArray = [];
+      while (characterArray.length !== characterLimit) {
+        const choice = Math.floor(Math.random() * Math.floor(filteredCharactersResult.length));
+
+        if (!characterArray.includes(filteredCharactersResult[choice])) {
+          console.log(`Number: ${choice}`);
+          console.log(`pushing ${filteredCharactersResult[choice].name}`);
+          characterArray.push(filteredCharactersResult[choice]);
+        }
+      }
+      characterFinalResult = characterArray;
+    } else {
+      characterFinalResult = filteredCharactersResult;
+    }
+
+    res.send({
+      status: 'success',
+      results: {
+        artifacts: artifactFinalResult,
+        characters: characterFinalResult,
+      },
+    }).status(200);
+  }
 });
 
 app.listen(port, () => {
